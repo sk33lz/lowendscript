76c9f2b1e40a585fa13043f2f8afeaec716383e3
Jason Moore <sk33lz@gmail.com>
2012-10-11 17:46:00 -0400

    reverting nginx changes

--- setup-debian.sh
+++ setup-debian.sh
@@ -141,9 +141,6 @@ END
 }
 
 function install_nginx {
-    sudo -s
-    nginx=stable # use nginx=development for latest development version
-    add-apt-repository ppa:nginx/$nginx
     apt-get update 
     apt-get install nginx
     
