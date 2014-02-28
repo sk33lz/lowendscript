f6b3ac486b7067f795ca0dbe85ded261c160f792
Asad Dhamani <dhamaniasad@gmail.com>
2014-02-28 20:56:58 +0530

    added locate to system

--- setup-debian.sh
+++ setup-debian.sh
@@ -83,6 +83,10 @@ function install_smart {
 	check_install smartmontools smartmontools
 }
 
+function install_locate {
+    check_install locate locate
+}
+
 function install_dropbear {
     check_install dropbear dropbear
     check_install /usr/sbin/xinetd xinetd
@@ -722,6 +726,7 @@ system)
     install_dropbear
 	install_htop
 	install_smart
+	install_locate
     ;;
 htmlsite)
     install_htmlsite $2
