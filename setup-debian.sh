73392b8b4be99880753cbeb1a2bab4141c6ada59
dhamaniasad <dhamaniasad@gmail.com>
2014-03-20 16:08:18 +0530

    Update setup-debian.sh

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 function check_install {
     if [ -z "`which "$1" 2>/dev/null`" ]
     then
